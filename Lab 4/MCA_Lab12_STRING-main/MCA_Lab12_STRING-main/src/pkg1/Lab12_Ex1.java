ae837a4786c2c36fb65e8eb9cbb9882e90586c2c
Hammad <hammadsiddiqui1212@gmail.com>
2022-09-05 15:52:56 +0530

    Changes by Hammad

--- Lab 4/MCA_Lab12_STRING-main/MCA_Lab12_STRING-main/src/pkg1/Lab12_Ex1.java
+++
@@ -1,9 +0,0 @@
-package pkg1;
-
-public class Lab12_Ex1 {
-	
-public static void main(String[] args) {
-System.out.println("My name is md Hassan");
-System.out.println("500094457"); 
-	}
-}
--- Lab 4/MCA_Lab12_STRING-main/MCA_Lab12_STRING-main/src/pkg1/mdhassan500094457.java
+++
@@ -1,6 +0,0 @@
-class Main{
-    public static void main(String[] args) {
-       System.out.println(" Md HASSAN");
-       System.out.println(" 500094457");
-    }
-}
